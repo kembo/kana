d3e25d233267bfdb8bf18a4c334222d2e5cb8d63
kembo <n.kembo@gmail.com>
2021-12-23 21:52:41 +0900

    Add inputting Yo-on

--- src/states.ts
+++ src/states.ts
@@ -16,17 +16,22 @@ function createStatesRec(...tree: StateTree): AcceptableState {
     return new AcceptableState(head, subState(norm), subState(rev));
 }
 
+function createYoon(iK: string): AcceptableState {
+    return createStatesRec(iK, [iK + 'ゃ', [iK + 'ゅ', iK + 'ょ']]);
+}
 /**
  * あ～お段を持つことを想定した行を返す関数
  * @param gyo その行の仮名文字一覧
  * @returns 先頭(あ段)の仮名を示す `State`
  */
-function createGyoStates(gyo: string): AcceptableState {
+function createGyoStates(gyo: string, yoon?: boolean): AcceptableState {
     if (gyo.length == 3) {
         return createStatesRec(gyo[0], [gyo[1], gyo[2]]);
     }
     if (gyo.length == 5) {
-        return createStatesRec(gyo[0], [gyo[2], gyo[4], gyo[3]], gyo[1]);
+        return createStatesRec(
+            gyo[0], [gyo[2], gyo[4], gyo[3]],
+            yoon ? createYoon(gyo[1]) : gyo[1]);
     }
     throw new Error("Gyo length must be 3 or 5!");
 }
@@ -34,30 +39,31 @@ function createGyoStates(gyo: string): AcceptableState {
 /** 仮名一覧(行ごと) */
 const KANAS_LIST = {
     a: createGyoStates('あいうえお'),
-    k: createGyoStates('かきくけこ'),
-    g: createGyoStates('がぎぐげご'),
-    s: createGyoStates('さしすせそ'),
-    z: createGyoStates('ざじずぜぞ'),
-    t: createGyoStates('たちつてと'),
-    d: createGyoStates('だぢづでど'),
-    n: createGyoStates('なにぬねの'),
-    h: createGyoStates('はひふへほ'),
-    p: createGyoStates('ぱぴぷぺぽ'),
-    b: createGyoStates('ばびぶべぼ'),
-    m: createGyoStates('まみむめも'),
-    y: createGyoStates('やゆよ'),
-    r: createGyoStates('らりるれろ'),
-    w: createGyoStates('わをん')
+    k: createGyoStates('かきくけこ', true),
+    g: createGyoStates('がぎぐげご', true),
+    s: createGyoStates('さしすせそ', true),
+    z: createGyoStates('ざじずぜぞ', true),
+    t: createGyoStates('たちつてと', true),
+    d: createGyoStates('だぢづでど', true),
+    n: createGyoStates('なにぬねの', true),
+    h: createGyoStates('はひふへほ', true),
+    p: createGyoStates('ぱぴぷぺぽ', true),
+    b: createGyoStates('ばびぶべぼ', true),
+    m: createGyoStates('まみむめも', true),
+    y: createStatesRec('や', ['ゆ', 'よ', ['ゃ', ['ゅ', 'ょ']]], ['（', '）', '　']),
+    r: createGyoStates('らりるれろ', true),
+    w: createStatesRec('わ', ['を', 'ん', 'ゎ'], ['、', ['。', '！', '？'], '：']),
+    x: createGyoStates('ぁぃぅぇぉ')
 } as const;
 console.log(KANAS_LIST);
 
 class PreGyoState extends FollowingState { }
 /** 行一覧 */
 const GYOES_LIST = {
-    a: new PreGyoState(KANAS_LIST.a),
+    a: new PreGyoState(KANAS_LIST.a, KANAS_LIST.x),
     k: new PreGyoState(KANAS_LIST.k, new PreGyoState(KANAS_LIST.g)),
     s: new PreGyoState(KANAS_LIST.s, new PreGyoState(KANAS_LIST.z)),
-    t: new PreGyoState(KANAS_LIST.t, new PreGyoState(KANAS_LIST.d)),
+    t: new PreGyoState(KANAS_LIST.t, new AcceptableState('っ', KANAS_LIST.d)),
     n: new PreGyoState(KANAS_LIST.n),
     h: new PreGyoState(KANAS_LIST.h, new PreGyoState(KANAS_LIST.b, KANAS_LIST.p)),
     m: new PreGyoState(KANAS_LIST.m, KANAS_LIST.y),
